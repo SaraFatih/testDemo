99b6328013c467b92bc2d27b9fea3d050d7a3fb2
Alexander Wingård <alexander.wingard@gmail.com>
2018-02-21 14:44:05 +0100

    Fix fix

--- MinTest.java
+++ MinTest.java
@@ -1,12 +1,11 @@
 import org.junit.* ;
+import static org.junit.Assert.*;
 public class MinTest {
 	
 		@Test public void test_find_min() {
 			int[] a = {5,1,7};
 			int res = Min.find_min(a);
-			if(res == 1){
-				System.out.print("Test successful\n");
-			}
+			assertEquals(res, 0);
 		} 
 
 }
